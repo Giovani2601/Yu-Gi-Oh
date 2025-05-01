ad212d3f0bd7cd2624a03f8eee3cfdc90df78880
Giovani2601 <giovanisousa2601@gmail.com>
2025-05-01 14:47:54 -0300

    Criando uma navbar para navegação

--- frontend-yugioh/src/App.js
+++ frontend-yugioh/src/App.js
@@ -1,23 +1,20 @@
 // src/App.js
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import './App.css'
+import Navbar from "./components/navbar/navbar"
 
-function Home()    { return <h2>🏠 Home</h2> }
-function About()   { return <h2>ℹ️ Sobre</h2> }
-function Yugioh() { return <h2>📖 Yu-Gi-Oh!</h2> }
+function Home()    { return <h2>Home</h2> }
+function Cards() { return <h2>Cards</h2> }
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <Link to="/">Home</Link> | <Link to="/About">Sobre</Link> | <Link to="/Yu-Gi-Oh">Yu-Gi-Oh!</Link>
-      </nav>
+      <Navbar/>
       <main className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Yu-Gi-Oh" element={<Yugioh />} />
+          <Route path="/cards" element={<Cards />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
---
+++ frontend-yugioh/src/components/navbar/navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import YuGiOhTitle from '../assets/yugioh-logo.png';
+import HomeLogo from '../assets/yugioh-home.png';          
+import CardsLogo from '../assets/yugioh-cards.png'; 
+import './navbar.css';
+
+export default function ResponsiveAppBar() {
+  return (
+    <AppBar position="static">
+      <Container className="bar" maxWidth="xl">
+        <Toolbar disableGutters sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ flex: 1, display: 'flex' }}>
+            <IconButton
+              component={Link}
+              to="/"
+              sx={{ p: 0 }}              
+              aria-label="Home"
+            >
+              <img
+                src={HomeLogo}
+                alt="Home"
+                className='homeLogo'
+              />
+            </IconButton>
+          </Box>
+          <Box sx={{ flex: 0 }}>
+            <Link to="/">
+              <img className="logo" src={YuGiOhTitle} alt="Yu-Gi-Oh Logo" />
+            </Link>
+          </Box>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+            <IconButton
+              component={Link}
+              to="/cards"
+              aria-label="Cards"
+            >
+              <img
+                src={CardsLogo}
+                alt="YuGiOh Cards"
+                className='cardsLogo'
+              />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
